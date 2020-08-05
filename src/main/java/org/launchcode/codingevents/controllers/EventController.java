5583eb060bcfef6fd16df078a62e4eb6aa8ecaa5
Denis Duarte <duarte.denis11@gmail.com>
2020-08-04 20:59:05 -0400

    Chapter 11 exercises complete.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,10 +18,13 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("LC101", "Javascript and Java learning on the fly!");
+        events.put("JPMC Code Academy", "10 weeks of intense Java basic training.");
+        events.put("Hackathon", "Do you have what it takes to compete against teams of other developers for all the glory?");
         model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
@@ -33,8 +37,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String description) {
+        events.put(eventName, description);
         return "redirect:";
     }
 
